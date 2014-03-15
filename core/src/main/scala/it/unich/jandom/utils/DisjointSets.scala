350091c725eeaaad20e29615611e41dec6fe123e
Gianluca Amato <gamato@unich.it>
2014-03-15 20:19:12 +0100

    Add a DisjointSet implementation in the util package.

---
+++ core/src/main/scala/it/unich/jandom/utils/DisjointSets.scala
@@ -0,0 +1,55 @@
+/**
+ * Copyright 2014 Gianluca Amato <gamato@unich.it>
+ *
+ * This file is part of JANDOM: JVM-based Analyzer for Numerical DOMains
+ * JANDOM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JANDOM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of a
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with JANDOM.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package it.unich.jandom.utils
+
+import scala.collection.generic.Growable
+
+/**
+ * This is a trait for disjoint sets. A disjoint set algorithm keeps a number
+ * of disjoint sets and allow to create new sets, join two sets, and find if
+ * two elements are in the same set.
+ */
+trait DisjointSets[T] extends Growable[T] {
+  /**
+   * Join the disjoint-sets of which <code>elem1</code>
+   * and <code>elem2</code> are members of. If <code>elem1</code>
+   * or <code>elem2</code> are not part of the disjoint sets, 
+   * they are added before performing the union.
+   * @return the representative of the unioned set 
+   */
+  def union(elem1: T, elem2: T): T
+
+  /**
+   * Finds the representative for a disjoint-set, of which
+   * <code>elem</code> is a member of.
+   */
+  def find(elem: T): Option[T]
+
+  /**
+   * Returns the number of disjoint-sets managed in this data structure.
+   */
+  def setCount: Int
+}
+
+/**
+ * This is the compaion object for the trait DisjointSets
+ */
+object DisjointSets {
+  def apply[T](initialElements: T*) = new DisjointSetsImpl[T](initialElements)
+}
